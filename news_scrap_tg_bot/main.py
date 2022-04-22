0386feaa53f66195664b6397a1a4381f7517f1c7
mrursu <mr0ursu@gmail.com>
2022-04-22 19:01:39 +0200

    add edition

--- news_scrap_tg_bot/main.py
+++ news_scrap_tg_bot/main.py
@@ -1,1 +1,29 @@
-print('hello')
+from operator import imod
+from urllib import response
+from wsgiref import headers
+import requests
+from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
+from datetime import datetime
+import time
+
+
+def get_first_news():
+
+    ua = UserAgent()
+    headers = {
+        'User-Agent': ua.random
+    }
+
+    url = 'https://www.securitylab.ru/news/'
+    response = requests.get(url = url, headers = headers)
+    soup = BeautifulSoup(response.text, 'lxml')
+    
+
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
